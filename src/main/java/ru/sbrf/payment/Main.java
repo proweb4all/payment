793443398dd62eb6ea1752ceae5401b0a59feede
proweb4all <proweb4all@narod.ru>
2020-10-14 16:43:15 +0300

    small update: added IoC

--- src/main/java/ru/sbrf/payment/Main.java
+++ src/main/java/ru/sbrf/payment/Main.java
@@ -4,25 +4,21 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.sbrf.payment.app.WebApp;
 import ru.sbrf.payment.server.BaseServer;
-import ru.sbrf.payment.server.ServerBank1;
-//import ru.sbrf.payment.server.ServerBank2;
-//import ru.sbrf.payment.server.ServerDefault;
+// import ru.sbrf.payment.server.ServerBank1;
+// import ru.sbrf.payment.server.ServerBank2;
 
 public class Main {
     public static void main(String[] args) {
 
 //        // Обычная реализация
 //        WebApp app = new WebApp();
-//        ServerBank1 serverBank1 = new ServerBank1(); // Можно закомментировать, app будет запускаться, но без серверного функционала
-// // //        ServerBank2 serverBank2 = new ServerBank2(); // Пример другого сервера, можно подключить его
-// // //        ServerDefault serverDefault = new ServerDefault(); // Пример сервера с дефолтным (никаким) функционалом
+//        BaseServer serverBank = new ServerBank1(); // Можно закомментировать, app будет запускаться, но без серверного функционала
+// // //        ... new ServerBank2(); // Пример другого сервера, можно подключить его
 
 //        // Spring реализация
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
         WebApp app = context.getBean("webapp", WebApp.class);
-        ServerBank1 serverBank1 = context.getBean("server1", ServerBank1.class);
-// // //        ServerBank2 serverBank2 = context.getBean("server2", ServerBank2.class);
-// // //        ServerDefault serverDefault = context.getBean("serverDefault", ServerDefault.class);
+        BaseServer serverBank = context.getBean("serverBank", BaseServer.class);
 
         app.runApp();
 
