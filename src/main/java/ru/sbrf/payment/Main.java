f535adc187eceabc49c04defbb5b2d519eaff927
proweb4all <proweb4all@narod.ru>
2020-10-16 14:11:03 +0300

    configuration of the spring application with annotations

--- src/main/java/ru/sbrf/payment/Main.java
+++ src/main/java/ru/sbrf/payment/Main.java
@@ -1,26 +1,35 @@
 package ru.sbrf.payment;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.sbrf.payment.app.WebApp;
 import ru.sbrf.payment.server.BaseServer;
 // import ru.sbrf.payment.server.ServerBank1;
 // import ru.sbrf.payment.server.ServerBank2;
 
+@ComponentScan(basePackages = "ru.sbrf.payment")
 public class Main {
     public static void main(String[] args) {
 
-//        // Обычная реализация
+//        // ========== Обычная реализация
 //        WebApp app = new WebApp();
 //        BaseServer serverBank = new ServerBank1(); // Можно закомментировать, app будет запускаться, но без серверного функционала
 // // //        ... new ServerBank2(); // Пример другого сервера, можно подключить его
 
-//        // Spring реализация
-        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        WebApp app = context.getBean("webapp", WebApp.class);
-        BaseServer serverBank = context.getBean("serverBank", BaseServer.class);
-        app.runApp();
-        BaseServer serverBank2 = context.getBean("serverBank2", BaseServer.class);
+//        // ========== Spring реализация
+
+//        // XML-конфигурация
+//        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+
+//        // Annotation-конфигурация
+        ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+
+        WebApp app = context.getBean("webApp", WebApp.class);
+        BaseServer serverBank1 = context.getBean("serverBank1", BaseServer.class);
+//        BaseServer serverBank2 = context.getBean("serverBank2", BaseServer.class);
+
         app.runApp();
 
     }
